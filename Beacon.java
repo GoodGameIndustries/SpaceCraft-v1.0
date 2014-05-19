19ab2923dd3eb25ce2afe1e155738402f46ef5fb
EmmettGGI <goodgameindustries@gmail.com>
2014-05-19 17:31:33 -0400

    Current Up To Date Code
    This is what we have to date

---
+++ Beacon.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author Emmett Deen
+ * @version 1.0
+ * @date May 14, 2014
+ */
+public class Beacon extends SpaceOBJ{
+	
+	public Beacon(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+}
---
+++ Sniper.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * 
+ */
+
+/**
+ * @author Emmett Deen
+ * @version 1.0
+ * @date May 17, 2014
+ */
+public class Sniper extends AttackShip{
+
+	
+	public Sniper(int x, int y, boolean player) {
+		super(x, y,35,35, player);
+		power = 10;
+		speed = 4;
+		health = 175;
+		name = "Sniper";
+	}
+	
+	public void paintComponent(Graphics g){
+		g.setColor(new Color(0,50,0));
+		g.fillOval(0,0,35,35);
+	}
+}
