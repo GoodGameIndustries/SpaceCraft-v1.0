6405ca90d28ef1a28c390aa0c6237ffbf1fa6db9
StevenGGI <steven@stevenliangs-MacBook-Air.local>
2014-05-20 12:27:15 -0400

    AI update with getMotherShip()

--- SpaceCraft/SpaceCraft 1.0/src/AI/AI.java
+++ SpaceCraft/SpaceCraft 1.0/src/AI/AI.java
@@ -1,5 +1,19 @@
 package AI;
 
+import java.awt.Color;
+
+import Frame.Space;
+import Ships.MotherShip;
+
 public class AI {
+	protected MotherShip mothership;
+	
+	public AI(int x, int y, Space space){
+		mothership=new MotherShip(x,y,space,Color.red);
+	}
+	
+	public MotherShip getMotherShip(){return mothership;}
+	
+	
 	
 }
